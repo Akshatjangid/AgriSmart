55793f428404a99f96d56a454d03886fe4b10c9e
Akshat <akshatjangid9@gmail.com>
2025-08-29 18:21:20 +0530

    update file

--- app.py
+++ app.py
@@ -154,5 +154,7 @@ def predict_disease():
         return render_template('result.html', disease=f"⚠️ Error: {e}")
 
 # --- 6. Run the App ---
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=False, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
